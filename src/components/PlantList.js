05c6860c1a808bbd4519a35214ac966e6b7db7f3
Pauline122112 <pauline.design007@gmail.com>
2021-08-06 07:51:47 -0500

    added state to the property called plants

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      plants: []
+    }
+  }
   // add state with a property called "plants" - initialize as an empty array
 
   // when the component mounts:
